22166c200589266f5cd3021109062a87e3601672
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-04-08 16:23:43 +0600

    Update UI & UX

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,11 @@ const Navbar = () => {
           <li onClick={() => window.location.reload('#Intro')}><a href=''>Home</a></li>
           <li onClick={() => window.location.replace("#Skill")}><a href='#Skill'>Skills</a></li>
           <li><a href='#Projects'>Projects</a></li>
-          <li><a href='#Experience'>Experience</a></li>
+          <li><a href='#Experience'>Experiences</a></li>
           <li><a href='#Education'>Education</a></li>
-          <li><a href='#Course'>Course</a></li>
-          <li><a href='#Test'>Test</a></li>
+          <li><a href='#Course'>Courses</a></li>
+          <li><a href='#Language'>Languages</a></li>
+          <li><a href='#Test'>Tests</a></li>
           <li><a href='#HireMe'>Hire Me</a></li>
           <li><a href='#ContactMe'>Contact Me</a></li>
         </ul>
