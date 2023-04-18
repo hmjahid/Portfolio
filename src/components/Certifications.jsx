59161704ca879c7d7163b37a46733f557f03158a
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-04-18 22:59:54 +0600

    Update Certifications.jsx => Add new certificates and details

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -8,10 +8,33 @@ const Certifications = () => {
         
         <section className="CertificationsSection">
         <h2 style={{color: "white", fontSize: "32px"}}><u>Certifications</u> :</h2>
+
+        <h3>Cyber Security Awareness For Everyone</h3>
+        <h4> * April 2023 *</h4>
+        <h5>Udemy</h5>
+        <a href="https://www.udemy.com/certificate/UC-0a194f0f-9644-4921-936d-1e35e012bc0b/" target="_blank"><button>Show Credential</button></a>
+
+        <br/>
+        <br/>
+        <h3>Fundamental of Embedded Systems</h3>
+        <h4> * April 2023 *</h4>
+        <h5>Udemy</h5>
+        <a href="https://www.udemy.com/certificate/UC-30045306-7a5f-412b-a2b9-2dfaeff9cab4/" target="_blank"><button>Show Credential</button></a>
+
+        <br/>
+        <br/>
+        <h3>Introduction to Artificial Intelligence in the Workplace</h3>
+        <h4> * April 2023 *</h4>
+        <h5>Udemy</h5>
+        <a href="https://www.udemy.com/certificate/UC-022e7655-fbfd-4b7e-915f-dad50b25f6a5/" target="_blank"><button>Show Credential</button></a>
+        
+        <br/>
+        <br/>
         <h3>Practical Git: for Absolute Beginners</h3>
         <h4> * April 2023 *</h4>
         <h5>Udemy</h5>
         <a href="https://www.udemy.com/certificate/UC-756f782d-1d2f-4fe5-ad40-db994351f9df/" target="_blank"><button>Show Credential</button></a>
+        
         </section>
     </div>
   );
