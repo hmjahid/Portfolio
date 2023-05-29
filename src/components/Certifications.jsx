346f3cd9c86a9e884fb4dad1ebf26af1222766a7
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-05-30 05:32:48 +0600

    Update Certifications.jsx => Add new certification

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -77,6 +77,16 @@ const Certifications = () => {
         <h5>Udemy</h5>
         <a href="https://www.udemy.com/certificate/UC-945332c7-6d33-4cd3-9084-8ffaf39c2e76/" target="_blank"><button>Show Credential</button></a>
         
+        <br/>
+        <br/>
+        <br/>
+        
+        <h3>Ultimate Wordpress Optimization 2022</h3>
+        <h4> * May 2023 *</h4>
+        <h5>Udemy</h5>
+        <a href="https://www.udemy.com/certificate/UC-c217ac3c-bf16-440e-af71-30c38533862e/" target="_blank"><button>Show Credential</button></a>
+        
+      
         </section>
     </div>
   );
